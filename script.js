a97ce4f817f7136cee97695b425607ebb0413679
abdulaziz27067011 <abdullox30007@gmail.com>
2025-02-26 19:39:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+let ism = prompt(`Ism kiriting`)
+let color = prompt(`Color kriting`)
+let bg = prompt(`BackgroundColor kiriting`)
+
+
+let matn = document.getElementById(`text`)
+let tana = document.querySelector(`body`)
+
+
+
+
+
+
+matn.style.color = color
+
+tana.style.backgroundColor = bg;
+
+
